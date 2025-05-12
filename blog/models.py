edd447207db155c7c7cd2fa3ff8a6d4d01ee67b9
seungmin <seungmin@maymust.com>
2025-05-12 19:01:21 +0900

    fix(hi): hi

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
+import os.path
+import os
+
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -10,10 +15,18 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author: 추후 작성 예정
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
-"""do_it_django_prj URL Configuration
+"""
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,9 +22,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("blog/", include("blog.urls")),
-    path("admin/", admin.site.urls),
-    path("", include("single_pages.urls")),
+    path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
 ]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
