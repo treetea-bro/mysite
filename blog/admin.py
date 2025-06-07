569727437ebadbafc1cb48ed4fc3253773c6597c
seungmin <seungmin@maymust.com>
2025-06-07 12:17:55 +0900

    feat(14주차): 중간저장

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category, Tag, Comment
+from markdownx.admin import MarkdownxModelAdmin
 
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
