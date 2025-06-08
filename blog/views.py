c52acdd2aa019dfe7ffc92ba69d011f0942fc69e
seungmin <seungmin@maymust.com>
2025-06-08 21:29:38 +0900

    feat(postDelete): post삭제 기능 추가 및 디자인 변경

--- blog/views.py
+++ blog/views.py
@@ -6,18 +6,32 @@ from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.user.is_authenticated and request.user == post.author:
+        post.delete()
+        return redirect("/blog/")
+    else:
+        raise PermissionDenied
+
 
 class PostList(ListView):
     model = Post
-    ordering = '-pk'
+    ordering = "-pk"
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data()
-        context['categories'] = Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context["categories"] = Category.objects.all()
+        context["no_category_post_count"] = Post.objects.filter(category=None).count()
 
         return context
+
 
 class PostDetail(DetailView):
     model = Post
@@ -32,30 +46,32 @@ class PostDetail(DetailView):
 
 
 def index(request):
-    posts = Post.objects.all().order_by('-pk')
+    posts = Post.objects.all().order_by("-pk")
 
     return render(
         request,
-        'blog/index.html',
+        "blog/index.html",
         {
-            'posts': posts,
-        }
+            "posts": posts,
+        },
     )
+
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        "blog/single_post_page.html",
         {
-            'post': post,
-        }
+            "post": post,
+        },
     )
 
+
 def category_page(request, slug):
-    if slug == 'no_category':
-        category = '미분류'
+    if slug == "no_category":
+        category = "미분류"
         post_list = Post.objects.filter(category=None)
     else:
         category = Category.objects.get(slug=slug)
@@ -63,14 +79,15 @@ def category_page(request, slug):
 
     return render(
         request,
-        'blog/post_list.html',
+        "blog/post_list.html",
         {
-            'post_list': post_list,
-            'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None).count(),
-            'category': category,
-        }
+            "post_list": post_list,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+            "category": category,
+        },
     )
+
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
@@ -78,25 +95,28 @@ def tag_page(request, slug):
 
     return render(
         request,
-        'blog/post_list.html',
+        "blog/post_list.html",
         {
-            'post_list': post_list,
-            'tag': tag,
-            'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None).count(),
-        }
+            "post_list": post_list,
+            "tag": tag,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+        },
     )
+
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
 
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+        if current_user.is_authenticated and (
+            current_user.is_staff or current_user.is_superuser
+        ):
             form.instance.author = current_user
             response = super(PostCreate, self).form_valid(form)
 
@@ -106,23 +126,24 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
                 tags_str = tags_str.replace(",", ";")
                 tags_list = tags_str.split(";")
 
-            for t in tags_list:
-                t = t.strip()
-                tag, is_tag_created = Tag.objects.get_or_create(name=t)
-                if is_tag_created:
-                    tag.slug = slugify(t, allow_unicode=True)
-                    tag.save()
-                self.object.tags.add(tag)
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
 
             return response
         else:
-            return redirect('/blog/')
+            return redirect("/blog/")
+
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
 
-    template_name = 'blog/post_update_form.html'
+    template_name = "blog/post_update_form.html"
 
     def get_context_data(self, **kwargs):
         context = super(PostUpdate, self).get_context_data()
@@ -130,7 +151,7 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             tags_str_list = list()
             for t in self.object.tags.all():
                 tags_str_list.append(t.name)
-            context['tags_str_default'] = '; '.join(tags_str_list)
+            context["tags_str_default"] = "; ".join(tags_str_list)
 
         return context
 
@@ -144,11 +165,11 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         response = super(PostUpdate, self).form_valid(form)
         self.object.tags.clear()
 
-        tags_str = self.request.POST.get('tags_str')
+        tags_str = self.request.POST.get("tags_str")
         if tags_str:
             tags_str = tags_str.strip()
-            tags_str = tags_str.replace(',', ';')
-            tags_list = tags_str.split(';')
+            tags_str = tags_str.replace(",", ";")
+            tags_list = tags_str.split(";")
 
             for t in tags_list:
                 t = t.strip()
@@ -160,11 +181,12 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
         return response
 
+
 def new_comment(request, pk):
     if request.user.is_authenticated:
         post = get_object_or_404(Post, pk=pk)
 
-        if request.method == 'POST':
+        if request.method == "POST":
             comment_form = CommentForm(request.POST)
             if comment_form.is_valid():
                 comment = comment_form.save(commit=False)
@@ -177,6 +199,7 @@ def new_comment(request, pk):
     else:
         raise PermissionDenied
 
+
 class CommentUpdate(LoginRequiredMixin, UpdateView):
     model = Comment
     form_class = CommentForm
@@ -186,6 +209,7 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
             return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
 
 def delete_comment(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
@@ -197,11 +221,12 @@ def delete_comment(request, pk):
     else:
         raise PermissionDenied
 
+
 class PostSearch(PostList):
     paginate_by = None
 
     def get_queryset(self):
-        q = self.kwargs['q']
+        q = self.kwargs["q"]
         post_list = Post.objects.filter(
             Q(title__contains=q) | Q(tags__name__contains=q)
         ).distinct()
@@ -209,7 +234,8 @@ class PostSearch(PostList):
 
     def get_context_data(self, **kwargs):
         context = super(PostSearch, self).get_context_data()
-        q = self.kwargs['q']
-        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+        q = self.kwargs["q"]
+        context["search_info"] = f"Search: {q} ({self.get_queryset().count()})"
 
         return context
+
